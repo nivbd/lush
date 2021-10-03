7f2c05b90d442f01cf4193ac9d55825a2b8d77b9
nbd <nivb@rapyd.net>
2021-10-03 22:09:48 +0300

    remove unused code

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -7,18 +7,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs) {
-  return { name, calories, fat, carbs };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24),
-  createData('Ice cream sandwich', 237, 9.0, 37),
-  createData('Eclair', 262, 16.0, 24),
-  createData('Cupcake', 305, 3.7, 67),
-  createData('Gingerbread', 356, 16.0, 49),
-];
-
 const TableComp = ({ headers, rows }) => {
   const renderHeaders = () => {
     return headers.map((headerData) => {
